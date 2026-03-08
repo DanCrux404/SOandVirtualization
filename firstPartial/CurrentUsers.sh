948bdfb5a6105e04815cc3fa3bbd2f76628b27c4
DanCrux404 <jorgedantex@gmail.com>
2026-03-08 14:33:07 -0600

    existing users

---
+++ firstPartial/CurrentUsers.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+echo "============================="
+echo "      Existed users"
+echo "============================="
+#Internal Field Separator IFS:= how to separate fields when reading
+#GECOS is a really ancient field that used to save user info
+#These days saves user info with format: Full name, ofice, work tel, home tel
+#tr -d ',' means translate delete — basically says: delete all characteres that are ,
+while IFS=: read -r USERNAME _ USER_ID GID_NUM GECOS HOME_DIR LOGIN_SHELL; do
+    GECOS=$(echo "$GECOS" | tr -d ',')
+    echo ""
+    echo "Usuario:       $USERNAME"
+    echo "UID:           $USER_ID"
+    echo "GID:           $GID_NUM"
+    echo "Nombre:        $GECOS"
+    echo "Home:          $HOME_DIR"
+    echo "Shell:         $LOGIN_SHELL"
+    echo "-----------------------------"
+done < <(getent passwd)
